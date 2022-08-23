4b68f152124774d5cf9bfc564ca959e4bebb5f60
Azif Uldanov <begimot02@mail.ru>
2022-08-23 13:38:36 +0200

    Refactoring

--- src/Game.java
+++ src/Game.java
@@ -5,7 +5,6 @@ public class Game {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        int storage=0;
 
         Stack<Integer> originalStack = new Stack<>();
         Stack<Integer> stack1 = new Stack<>();
@@ -22,41 +21,45 @@ public class Game {
         System.out.println("Начинаем");
 
 
-        while ((!stack2.equals(originalStack) &&(!stack3.equals(originalStack)))) {
+        while ((!stack2.equals(originalStack) && (!stack3.equals(originalStack)))) {
             System.out.println("введите номера кучи 1- № снимаемой, 2- № куда ложить ");
             int original = scanner.nextInt();
             int finite = scanner.nextInt();
 
-           storage = takeOffToHeap(stack1, stack2, stack3,original);
-            addToHeap(stack1,stack2,stack3,finite,storage);
-         //   storage=0;
+            int storage = takeOffToHeap(stack1, stack2, stack3, original);
+            addToHeap(stack1, stack2, stack3, finite, storage);
+
             System.out.println(stack1);
             System.out.println(stack2);
             System.out.println(stack3);
         }
         System.out.println(" Вы закончили игу");
     }
-    public static void addToHeap(Stack<Integer> stack1,Stack<Integer> stack2,Stack<Integer> stack3,int x, int z ){
-        switch (x){
-            case 1:stack1.push(z);
-            break;
-            case 2:stack2.push(z);
-            break;
-            case 3:stack3.push(z);
-        }
-    }
-    public static int takeOffToHeap(Stack<Integer> stack1, Stack<Integer> stack2, Stack<Integer> stack3, int x){
-        int storage=0;
-        switch (x){
+
+    public static void addToHeap(Stack<Integer> stack1, Stack<Integer> stack2, Stack<Integer> stack3, int x, int z) {
+        switch (x) {
             case 1:
-                storage= stack1.pop();
+                stack1.push(z);
                 break;
             case 2:
-                storage= stack2.pop();
+                stack2.push(z);
                 break;
             case 3:
-                storage = stack3.pop();
+                stack3.push(z);
         }
-        return storage;
+    }
+
+    public static int takeOffToHeap(Stack<Integer> stack1, Stack<Integer> stack2, Stack<Integer> stack3, int x) {
+
+        switch (x) {
+            case 1:
+                return stack1.pop();
+            case 2:
+                return stack2.pop();
+            case 3:
+                return stack3.pop();
+            default:
+                return 0;
+        }
     }
 }
